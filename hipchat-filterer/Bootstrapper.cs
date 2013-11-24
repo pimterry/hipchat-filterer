7b07f5bb23b67f9cf973560366584d19742d47b1
pimterry <pimterry@gmail.com>
2013-11-24 23:44:12 +0000

    Added Elmah

---
+++ hipchat-filterer/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Elmah;
+using Nancy.TinyIoc;
+
+namespace hipchat_filterer
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            Elmahlogging.Enable(pipelines, "elmah");
+        }
+    }
+}
